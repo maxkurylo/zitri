99763d6b277fac1af1594fa19794234db9eac1c6
maxkurylo <kurylo.max@gmail.com>
2022-02-05 21:54:49 +0200

    Fix chat height

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {UsersService} from "./services/users.service";
 import {ChatService} from "./services/chat.service";
 import { isMobile } from "./helpers";
@@ -10,12 +10,17 @@ import { isMobile } from "./helpers";
 })
 export class AppComponent implements OnInit {
     isMobile = false;
+    chatHeight = '400px';
+    @ViewChild('chatContainer', { static: false }) private chatContainer: ElementRef;
+
 
     constructor(public us: UsersService, public cs: ChatService) {
         this.isMobile = isMobile() || window.innerWidth < 768;
+        this.chatHeight = window.innerHeight - 20 + 'px';
 
         window.addEventListener('resize', () => {
             this.isMobile = isMobile() || window.innerWidth < 768;
+            this.chatHeight = window.innerHeight - 20 + 'px';
         });
     }
 
