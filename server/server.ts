9800d6628b80396052850d90a4a866c70a36b73c
maxkurylo <kurylo.max@gmail.com>
2022-10-09 16:17:40 +0300

    Handle non-existing assets

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,15 @@
 // Prerequisites
 if (process.env.NODE_ENV === 'development') {
+    // tslint:disable-next-line:no-var-requires
     require('dotenv').config();
 }
 if (!process.env.JWT_SECRET || !process.env.ROOM_SECRET) {
-    throw 'App secrets are missing! Add ROOM_SECRET and JWT_SECRET environment variables'
+    throw new Error('App secrets are missing! Add ROOM_SECRET and JWT_SECRET environment variables');
 }
 
 // external imports
 import http from 'http';
-import express from 'express';
+import express, {Request, Response} from 'express';
 import cors from 'cors';
 import compression from 'compression';
 import path from 'path';
@@ -16,7 +17,7 @@ import bodyParser from 'body-parser';
 import passport from 'passport';
 
 // own modules imports
-import sslRedirect from './modules/ssl-redirect'
+import forceProductionSSL from './modules/ssl-redirect'
 import WebSockets  from './modules/sockets';
 import authApi from './api/auth';
 import roomsApi from './api/rooms';
@@ -36,7 +37,7 @@ const app = express();
 const server = http.createServer(app);
 
 // app configuration
-app.use(sslRedirect);
+app.use(forceProductionSSL);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
@@ -57,8 +58,11 @@ app.use('/api', roomsApi);
 // serve static files
 app.use(express.static(path.join(__dirname, CLIENT_DIST_DIRECTORY)));
 
+// send 404 if assets do not exist
+app.get('/assets/*', (req: Request, res: Response) => { res.sendStatus(404); });
+
 // Handle Angular routing
-app.get('*', (req: any, res: any) => { res.sendFile(CLIENT_INDEX_PATH); });
+app.get('*', (req: Request, res: Response) => { res.sendFile(CLIENT_INDEX_PATH); });
 
 // start listening
 server.listen(PORT, () => console.log(`Backend listening on port ${PORT}!`));
